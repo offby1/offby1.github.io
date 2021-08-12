d3e12eebf4dc2d9da69dfd129ee000fca2fc093a
Eric Hanchrow <eric.hanchrow@gmail.com>
2021-08-11 18:42:30 -0700

    Another favorite

---
+++ chicken-nuggets.html.pm
@@ -0,0 +1,24 @@
+#lang pollen
+
+◊center{Chicken Nuggets}
+◊center{From "The Wire" Season One}
+◊author{David Simon et al}
+◊center{Wallace} Man, these shits is right, yo.
+◊center{Poot} Mmm hmm!
+◊center{Wallace} Good with the hot sauce, too!
+◊center{Poot} Most definitely.
+◊center{Wallace} Yo D -- you want some nuggets?
+◊center{D'Angelo} Nah, go ahead, man.
+◊center{Wallace} Man.  Whoever invent these -- he off the hook.
+◊center{Poot} Whaaat?
+◊center{Wallace} Motherfucker get the bone all the way ◊em{out} the damned chicken.  'Til he came along, niggers be chewing on drumsticks and shit, getting they fingers all greasy.  He said: "Later for the bone.  Let's nugget that meat up and make some real money".
+◊center{Poot} You think the man got paid?
+◊center{Wallace} Who?
+◊center{Poot} The man who invented these.
+◊center{Wallace} Shit.  He richer than a motherfucker.
+◊center{D'Angelo} Why.  You think he get a percentage?
+◊center{Wallace} Why not?
+◊center{D'Angelo} Nigga please.  The man who invented them things?  He just some sad-ass down at the basement of McDonald's.  Thinking up some shit to make some money for the real players.
+◊center{Poot} Naw, man, that ain't right.
+◊center{D'Angelo} Fuck right.  It ain't about right.  It's about money.  Now, you think Ronald McDonald gonna go down that basement and say "Hey Mr Nugget!  You da bomb!  We selling chicken faster'n you can tear the bone out.  So I'm gonna write my clowny-ass name on this fat-ass check for you."  Shit.  And the nigger who invented those things?  He still workin in the basement for regular wage, thinking of some shit to make the fries taste better or some shit like that.  Believe.
+◊center{Wallace} Still had the idea, though.
